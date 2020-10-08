01c56314b4d7301735f5266185db704bd5d7fcaf
Edder <edhernandezfo@unal.edu.co>
2020-10-07 23:45:20 -0500

    delete css class :fire: gitignore :see_no_evil:

--- js/documentation.js
+++ js/documentation.js
@@ -1,27 +1,47 @@
+//Collapse presentations
 $("#presentacion").click(function () {
     $("#video").collapse('hide');
     $("#pdf").collapse('show');
 
-    console.log("BOOLEAN", isExpandedPDF)
-
 });
+//Collapse videos
 $("#videos").click(function () {
     $("#pdf").collapse('hide');
     $("#video").collapse('show');
 });
-$("#submission-ml").click(function () {
-    $(".docCollapse").collapse('hide');
-});
 
-$('.docAux').click(function () {
-    let id = $(this).attr('id');
-    $(".docCollapse").collapse('hide');
-    $(`#${id}-v`).collapse('show');
-    console.log(id);
-});
-$('.docAux2').click(function () {
-    let id = $(this).attr('id');
-    $(".docCollapse2").collapse('hide');
+//Collapse pdf
+$('#pdf-list li').click(function(){
+    let IDs = []
+    $('#pdf-list').find("a").each(function (){IDs.push(this.id)});
+    let id = $(this).find('a').attr('id');
+
+    IDs = $.grep(IDs,function(value){
+        return value !== id;
+    });
+    
+    $.each(IDs,function (i,l){
+        console.log(l);
+        $(`#${l}-pdf`).collapse('hide');
+    });
     $(`#${id}-pdf`).collapse('show');
-    console.log(id);
+    
+});
+//Collapse videos
+$('#videos-list li').click(function(){
+    let IDs = [];
+    $('#videos-list li').find("a").each(function (){IDs.push(this.id)});
+    let id = $(this).find('a').attr('id');
+   
+    console.log("IDS:", IDs);
+    IDs = $.grep(IDs,function(value){
+        return value !== id;
+    });
+    console.log("IDS2:", IDs);
+    $.each(IDs,function (i,l){
+        $(`#${l}-v`).collapse('hide');
+        console.log("hide",l);
+    });
+    $(`#${id}-v`).collapse('show');
+    console.log("id",id);
 });
