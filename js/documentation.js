3a7b6f0955b605c187f55bd8202582eb1ebaaf95
Edder <edhernandezfo@unal.edu.co>
2020-10-08 00:57:49 -0500

    move slides about UNCODE's paper to publications and new guide slide downloadable in documentations

--- js/documentation.js
+++ js/documentation.js
@@ -11,37 +11,40 @@ $("#videos").click(function () {
 });
 
 //Collapse pdf
-$('#pdf-list li').click(function(){
-    let IDs = []
-    $('#pdf-list').find("a").each(function (){IDs.push(this.id)});
+$('#pdf-list li').click(function () {
     let id = $(this).find('a').attr('id');
 
-    IDs = $.grep(IDs,function(value){
-        return value !== id;
-    });
-    
-    $.each(IDs,function (i,l){
-        console.log(l);
-        $(`#${l}-pdf`).collapse('hide');
-    });
-    $(`#${id}-pdf`).collapse('show');
-    
+    if ($('#noCollapse').find(`#${id}`).length !== 1) {
+        let IDs = []
+        $('#pdf-list').find("a").each(function () {
+            IDs.push(this.id)
+        });
+        IDs = $.grep(IDs, function (value) {
+            return value !== id;
+        });
+
+        $.each(IDs, function (i, l) {
+            $(`#${l}-pdf`).collapse('hide');
+        });
+        $(`#${id}-pdf`).collapse('show');
+    }
+
+
 });
 //Collapse videos
-$('#videos-list li').click(function(){
-    let IDs = [];
-    $('#videos-list li').find("a").each(function (){IDs.push(this.id)});
+$('#videos-list li').click(function () {
     let id = $(this).find('a').attr('id');
-   
-    console.log("IDS:", IDs);
-    IDs = $.grep(IDs,function(value){
+    let IDs = [];
+    $('#videos-list li').find("a").each(function () {
+        IDs.push(this.id)
+    });
+
+    IDs = $.grep(IDs, function (value) {
         return value !== id;
     });
-    console.log("IDS2:", IDs);
-    $.each(IDs,function (i,l){
+    $.each(IDs, function (i, l) {
         $(`#${l}-v`).collapse('hide');
-        console.log("hide",l);
     });
     $(`#${id}-v`).collapse('show');
-    console.log("id",id);
+
 });
