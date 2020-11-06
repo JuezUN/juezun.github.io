99d7231dda4940602d23a6aa7e5c828125110935
Heldeg <edhernandezfo@unal.edu.co>
2020-11-06 14:58:48 -0500

    Fix/minor content fixes (#7)
    * Change footer logo to be transparent
    
    * fix logo link
    
    * all external links open a new page
    
    * Fix errors on index
    
    * Fix errors on documentation, publications and repositories
    
    * Contributors ionic script
    
    * First show "presentaciones"
    
    * Comments and delete contributors.js file
    
    contributors.js was empty

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,8 @@
 $(function () {
+    /* Change header url for index page */
     $.get("pages/header.html", function (data) {
         let dataAux = $(data);
+        /* Get and change the nav list */
         let list = dataAux.find("#nav-list li");
         $(list).each(function () {
             let aLabel = $(this).find("a");
@@ -8,10 +10,15 @@ $(function () {
                 aLabel.attr("href", `pages/${aLabel.attr("href")}`);
             }
         });
+        /* Change the url for specific resources */
         let logo = dataAux.find("#un-h");
         logo.attr("src","assets/img/LogoUN.png");
         $("#header-index-id").html(dataAux);
+        let logoLink = dataAux.find("#logo-link");
+        logoLink.attr("href","index.html");
+
     });
+    /* Change footer url for index page */
     $.get("pages/footer.html", function (data){
         let dataAux = $(data);
         let tate = dataAux.find("#un-f");
